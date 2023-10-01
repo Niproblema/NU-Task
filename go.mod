347762a0d9ff1ddb7e576689057a071ad4ba72d0
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2023-10-01 09:40:58 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Niproblema/NU-Task
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	""
+	fb_feed "github.com/Niproblema/LFG-EventAccumulator/fb_feed"
+)
+
+func main() {
+	fb_feed.FetchSample()
+}
